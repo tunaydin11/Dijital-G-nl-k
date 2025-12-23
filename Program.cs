c2722e052dbf49ecea38c45d487e045e7065faf6
Tunahan <154346978+tunaydin11@users.noreply.github.com>
2025-12-23 15:45:00 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DijitalGunluk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dijital Günlük Projesi");
+        }
+    }
+}
